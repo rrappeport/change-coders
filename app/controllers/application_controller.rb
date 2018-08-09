a75d0b821ddc9949a798208751dcd3463c969689
Ali Alqattan <ali.alqattan25@gmail.com>
2018-08-09 13:19:25 +0800

    added devise views for developer and charity seperatly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
    # [...]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_developer!
+  # before_action :authenticate_charity!
+
   include Pundit
 
   def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    if resource_name == Developer
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:github_account, :birth_date, :profile_pic, :experience, :description, :address, :linkedin, :skills ])
+      # For additional in app/views/devise/registrations/edit.html.erb
+      devise_parameter_sanitizer.permit(:account_update, keys: [:github_account, :birth_date, :profile_pic, :experience, :description, :address, :linkedin, :skills ])
+    else
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # charity devise
+    end
+    # For additional fields in app/views/devise/registrations/new.html.erb
+
   end
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,4 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_developer!, only: [:show, :index]
+  before_action :authenticate_charity!, only: [:new, :create, :edit, :update]
 end
