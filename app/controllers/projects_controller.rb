35e6681ad22a8c0ad90883c86890a55278893764
Ania Czekaj <Akczekaj@gmail.com>
2018-08-09 14:52:15 +0800

    pundit solved

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,13 @@
 class ProjectsController < ApplicationController
   # before_action :authenticate_developer!, only: [:show, :index]
-  before_action :authenticate_charity!, only: [:new, :create, :edit, :update]
+  # before_action :authenticate_charity!, only: [:new, :create, :edit, :update]
 
-  def pundit_user
-    current_developer
-  end
+  # def pundit_user
+  #   current_developer
+  # end
 
   def index
+    byebug
     @projects = policy_scope(Project)
   end
   def show
