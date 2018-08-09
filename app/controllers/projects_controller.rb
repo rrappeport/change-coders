d5ce07ae17d5e98117631d8116f9c3e40f2ee7d0
Ali Alqattan <ali.alqattan25@gmail.com>
2018-08-09 16:27:20 +0800

    added autherize function for developer only, will add charity later

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,5 @@
 class ProjectsController < ApplicationController
-  # before_action :authenticate_developer!, only: [:show, :index]
-  # before_action :authenticate_charity!, only: [:new, :create, :edit, :update]
-
-  # def pundit_user
-  #   current_developer
-  # end
-
   def index
-    byebug
     @projects = policy_scope(Project)
   end
   def show
