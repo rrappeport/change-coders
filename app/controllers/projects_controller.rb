702ed693fe7f8dd7e86bd5d866d9d7c1054c619a
Ali Alqattan <ali.alqattan25@gmail.com>
2018-08-10 14:49:28 +0800

    added a filter to the projects index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,29 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = policy_scope(Project)
+      @projects = @projects.where(nil)
+      @projects = @projects.where(status: params[:status]) if params[:status].present?
+      @projects = @projects.where(work_type: params[:work_type]) if params[:work_type].present?
+      if params[:category].present?
+        @charities = Charity.where(category: params[:category])
+        @projects = @projects.where(charity_id: @charities.pluck(:id))
+      end
+      if params[:deadline].present?
+          @projects = @projects.where("deadline <= ? ", Date.today + params[:deadline].to_i.day)
+      end
+    # filtering_params(params).each do |key, value|
+    #   byebug
+    #   @projects = @projects.public_send(key, value) if value.present?
+    # end
   end
+
+  #  def index
+  #   if params[:query].present?
+  #     @movies = Movie.where(title: params[:query])
+  #   else
+  #     @movies = Movie.all
+  #   end
+  # end
 
   def show
     @project = Project.find(params[:id])
@@ -32,5 +54,9 @@ class ProjectsController < ApplicationController
   private
   def project_params
     params.require(:project).permit(:type, :status, :github, :deadline, :charity_id, :name)
+  end
+
+  def filtering_params(params)
+    params.slice(:status, :work_type, :category)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ApplicationRecord
+  scope :status, -> (status) { where status: status }
+  # scope :category, -> (charity.category_id) { where category_id: category_id }
+  # scope :work_type, -> (work_type) { where work_type: work_type }
+  # scope :deadline, -> (deadline) { where deadline: deadline }
+  # scope :status, -> (status) { where("status like ?", "%#{status}%")}
+  scope :work_type, -> (work_type) { where("work_type like ?", "%#{work_type}%")}
+  scope :charity_category, -> (charity_category) { where("charity_category like ?", "%#{charity_category}%")}
   belongs_to :charity
+  STATUSES = ['pending', 'active', 'done', 'In Progress']
+  TYPES = ['Rebuild', "New", "Update"]
+  CATEGORIES = ['Environment', "Community Development", "Animals"]
+  DEADLINES = [15, 30, 60]
 end
