4ddf8e0350614fcc503cf8a680aba1ebb4a43a14
Ali Alqattan <ali.alqattan25@gmail.com>
2018-08-11 15:20:59 +0800

    Developer can login with github account only

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :charities, path: 'charities'
-  devise_for :developers, :controllers => { :omniauth_callbacks => "callbacks" }, path: 'developers'
+  devise_for :developers , :controllers => { :omniauth_callbacks => "callbacks" }, path: 'developers'
   root to: 'pages#home'
 
   resources :projects do
