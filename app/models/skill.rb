b08e328c8f87e17c2e3456cea881c6962e3b1ed3
Ania Czekaj <Akczekaj@gmail.com>
2018-08-11 17:41:55 +0800

    removing skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,6 @@
 class Skill < ApplicationRecord
+  has_many :developer_skills
   has_many :developers, through: :developer_skills
+  has_many :project_skills
   has_many :projects, through: :project_skills
 end
