d4a4e18d83334dcffcf91a8603c6b2d618c4a3fe
Lena Plotnikova <plotman@gmail.com>
2018-08-13 17:25:52 +0800

    Added pundit logic for member - only dev-lead and charity can delete a member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,7 @@
 class MembersController < ApplicationController
   def destroy
     @member = Member.find_by(developer_id: params[:id])
+    authorize @member
     @member.destroy
     redirect_to projects_dashboard_path(params[:project_id])
     flash[:alert] = "#{Developer.find(@member.developer_id).first_name.capitalize} was removed from the project"
---
+++ app/policies/member_policy.rb
@@ -0,0 +1,8 @@
+class MemberPolicy
+
+  def destroy?
+    ((user.class == Developer) && (record.project.leader_id  == user.id)) || ((user.class == Charity) && (record.project.charity_id  == user.id))
+  end
+
+
+end
