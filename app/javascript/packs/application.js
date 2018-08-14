dfa6c1cd4b00b8103960ebba2493a44ca444fc94
Lena Plotnikova <plotman@gmail.com>
2018-08-14 17:54:36 +0800

    Navbar updated, added new logo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@ import "../plugins/flatpickr"
 import { initMap } from 'components/maps';
 // [...]
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+initUpdateNavbarOnScroll();
+
 import { autocomplete } from 'components/autocomplete';
 import { initSkillChoice } from 'components/skill_choice';
 // [...]
