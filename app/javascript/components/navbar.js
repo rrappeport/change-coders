d3ddc570bde02a12cb59d0d6cd6b9aac77fa884d
Ryan Rappeport <rrappeport@gmail.com>
2018-08-15 11:29:12 +0800

    Navbar background and size

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,7 @@ function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= (10)) {
         navbar.classList.add('navbar-wagon-white');
       } else {
         navbar.classList.remove('navbar-wagon-white');
